b8eee6a183f755770d89a283474afb2c24cd8fec
KyujinPark01 <rbwls1100@naver.com>
2026-05-01 14:30:29 +0900

    first commit

---
+++ src/main/java/com/EDF/EDF_BE/EdfBeApplication.java
@@ -0,0 +1,13 @@
+package com.EDF.EDF_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdfBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdfBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/EDF/EDF_BE/EdfBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EDF.EDF_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdfBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
